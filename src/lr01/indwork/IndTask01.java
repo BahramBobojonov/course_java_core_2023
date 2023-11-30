f4166cc2c98532f6917b22faa5187de167648e88
BahramBobojonov <bobojonoffff@gmail.com>
2023-11-30 23:02:19 +0300

    ind part lab1

---
+++ src/lr01/indwork/IndTask01.java
@@ -0,0 +1,17 @@
+package lr01.indwork;
+
+import java.util.Scanner;
+
+public class IndTask01 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите фамилию: ");
+        String name = in.nextLine();
+        System.out.println("Введите имя: ");
+        String name2 = in.nextLine();
+        System.out.println("Введите отчество: ");
+        String name3 = in.nextLine();
+        System.out.println("Hello " + name + ", " + name2 + ", " + name3);
+        in.close();
+    }
+}
