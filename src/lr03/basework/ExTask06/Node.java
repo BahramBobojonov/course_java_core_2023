4b7a1eb61ac9f4f5df2c065ab64523e70e19c984
BahramBobojonov <bobojonoffff@gmail.com>
2023-12-02 15:53:46 +0300

    complete base part lab3 commit 2

---
+++ src/lr03/basework/ExTask06/Node.java
@@ -0,0 +1,11 @@
+package lr03.basework.ExTask06;
+
+public class Node {
+    public int value;
+    public Node next;
+
+    Node(int value, Node next) {
+        this.value = value;
+        this.next = next;
+    }
+}
