9e7c61700a39d9c9a512e12d0a4b984bfb714574
BahramBobojonov <bobojonoffff@gmail.com>
2023-12-02 13:59:21 +0300

    complete base part lab3

---
+++ src/lr03/basework/ExTask01/Example1.java
@@ -0,0 +1,14 @@
+package lr03.basework.ExTask01;
+
+public class Example1 {
+    public static void m(int x) {
+        System.out.println("Значение x = " + x);
+        if ((2 * x + 1) < 20) {
+            m(2 * x + 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        m(1);
+    }
+}
